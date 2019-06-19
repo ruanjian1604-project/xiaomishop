2fedb8eb691e9846d91cccd0034af1615e687419
zhaoyuzu <1329422182@qq.com>
2019-06-19 20:41:51 +0800

    combline

--- Redmi donate activity/小米手机移动端/js/huoqu.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var html = document.documentElement;
-    var hWidth = html.getBoundingClientRect().width;
-    html.style.fontSize = hWidth / 15 + "px";
-    console.log(hWidth);
-})()
