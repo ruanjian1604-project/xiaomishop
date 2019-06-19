83e276f6bacd5314b2be2c82b8e96e985b8b322d
MinimuM1 <50944984+MinimuM1@users.noreply.github.com>
2019-06-19 14:57:58 +0800

    Update index.js

--- MIUI_seckill/js/index.js
+++ MIUI_seckill/js/index.js
@@ -15,57 +15,20 @@ function changeTab() {
   box[index].className = 'selected'
 }
 
-// 倒计时1
-var h = document.getElementById('hour');
-var m = document.getElementById('minute');
-var s = document.getElementById('second');
+// 倒计时
+var h = document.getElementsByClassName('hour');
+var m = document.getElementsByClassName('minute');
+var s = document.getElementsByClassName('second');
 count();
 setInterval(count, 1000);
-function count(){
+function count() {
   var date = +new Date();
-  var target = +new Date('2111-6-18 00:00:00');
-  var time = target - date;//毫秒 
-  time /= 1000;
-  var hour = Math.floor(time / 60 / 60 % 24);
-  var minute = Math.floor(time / 60 % 60);
-  var second = Math.floor(time % 60);
-  h.innerText = hour;
-  m.innerText = minute;
-  s.innerText = second;
-}
-// 倒计时2
-var h2 = document.getElementById('hour2');
-var m2 = document.getElementById('minute2');
-var s2 = document.getElementById('second2');
-count2();
-setInterval(count2, 1000);
-function count2(){
-  var date = +new Date();
-  var target = +new Date('2111-6-18 04:00:00');
-  var time = target - date;//毫秒 
-  time /= 1000;
-  var hour = Math.floor(time / 60 / 60 % 24);
-  var minute = Math.floor(time / 60 % 60);
-  var second = Math.floor(time % 60);
-  h2.innerText = hour;
-  m2.innerText = minute;
-  s2.innerText = second;
-}
-// 倒计时3
-var h3 = document.getElementById('hour3');
-var m3 = document.getElementById('minute3');
-var s3 = document.getElementById('second3');
-count3();
-setInterval(count3, 1000);
-function count3(){
-  var date = +new Date();
-  var target = +new Date('2111-6-18 10:00:00');
-  var time = target - date;//毫秒 
-  time /= 1000;
-  var hour = Math.floor(time / 60 / 60 % 24);
-  var minute = Math.floor(time / 60 % 60);
-  var second = Math.floor(time % 60);
-  h3.innerText = hour;
-  m3.innerText = minute;
-  s3.innerText = second;
+  var target = new Array(+new Date('2019-6-20 11:30:00'), +new Date('2019-6-20 12:00:00'), +new Date('2019-6-20 16:00:00'));
+  var time = new Array();
+  for (var i = 0; i < target.length; i++) {
+    time[i]=(target[i]-date)/1000;
+    h[i].innerText = Math.floor(time[i] / 60 / 60);
+    m[i].innerText = Math.floor(time[i] / 60 % 60);
+    s[i].innerText = Math.floor(time[i] % 60);
+  }
 }
